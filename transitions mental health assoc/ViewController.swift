7a9aa4e48fe4c568a946c82835852210c786dc0b
Finlay <fpiroth@calpoly.edu>
2020-01-07 15:12:50 -0800

    Initial Commit

---
+++ transitions mental health assoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  transitions mental health assoc
+//
+//  Created by Finlay Piroth on 1/7/20.
+//  Copyright © 2020 Hack4Impact. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
