f24a334f7cc74d9a8cb35b045a513695ef9da4d3
Finlay <fpiroth@calpoly.edu>
2020-04-05 11:59:25 -0700

    some ui changes

--- transitions mental health assoc/ViewController.swift
+++ transitions mental health assoc/ViewController.swift
@@ -14,9 +14,17 @@ class ViewController: UIViewController {
     //CELLS -> text info, image
     //give buttons functionality
     
+    //MARK: properties
+    @IBOutlet weak var dashboardButton: UIButton!
+    @IBOutlet weak var messagesButton: UIButton!
+    @IBOutlet weak var patientDataButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setButton(button: dashboardButton)
+        setButton(button: messagesButton)
+        setButton(button: patientDataButton)
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         
         view.addGestureRecognizer(tap)
@@ -25,5 +33,29 @@ class ViewController: UIViewController {
     @objc func dismissKeyboard (){
         view.endEditing(true)
     }
+    
+    func setButton(button: UIButton){
+        let imgsize = button.imageView!.frame.size
+        let titlesize = button.titleLabel!.frame.size
+        let totalHeight = imgsize.height + titlesize.height
+
+        button.imageEdgeInsets = UIEdgeInsets(top: -(totalHeight - imgsize.height), left: 42.5 - (imgsize.width/2), bottom: 0, right: 0)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: -imgsize.width, bottom: -(totalHeight - titlesize.height), right: 0)
+    }
+}
+
+class Appointment {
+    var type: String
+    var priority: String
+    var date: String
+    var time: String
+    
+    init(type: String, priority: String, date: String, time: String) {
+        self.type = type
+        self.priority = priority
+        self.date = date
+        self.time = time
+    }
+    
 }
 
