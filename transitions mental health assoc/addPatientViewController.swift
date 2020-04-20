1fb7079bf350a923ce9a365da740681873301de1
Finlay <fpiroth@calpoly.edu>
2020-04-20 16:33:53 -0700

    front end: form finished, still some bugs need to be fixed. back end: http post working for the most part

---
+++ transitions mental health assoc/addPatientViewController.swift
@@ -0,0 +1,30 @@
+//
+//  addPatientViewController.swift
+//  transitions mental health assoc
+//
+//  Created by Finlay Piroth on 4/14/20.
+//  Copyright © 2020 Hack4Impact. All rights reserved.
+//
+
+import UIKit
+
+class addPatientViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
